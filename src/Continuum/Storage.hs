5f6fa3e3f900ccabeb11bd39a26bf8e742f8c07a
Alex Petrov <alexp@coffeenco.de>
2014-07-26 21:43:10 +0200

    Add serialization for basic types

--- src/Continuum/Storage.hs
+++ src/Continuum/Storage.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Continuum.Storage where
 
 import           Control.Applicative ((<$>))
 import           Control.Monad.Trans.Resource
+import           Data.Serialize (Serialize, encode, decode)
 import qualified Database.LevelDB.Base  as Base
 import           Database.LevelDB.Base (Options, DB, WriteOptions, ReadOptions)
+import           GHC.Generics
 
 import           Data.ByteString              (ByteString)
 
 
-import Control.Monad.Reader
+import           Control.Monad.Reader
 
 
 type RWOptions = (ReadOptions, WriteOptions)
@@ -32,6 +35,13 @@ db = asks ctxDb
 keyspace :: MonadReader DBContext m => m ByteString
 keyspace = asks ctxKeyspace
 
+
+data DbValue = Int |
+               String --- or opt-out for DbInt / DBString?
+             deriving (Eq, Ord, Generic)
+
+instance Serialize DbValue
+
 rwOptions :: MonadReader DBContext m => m RWOptions
 rwOptions = asks ctxRwOptions
 
@@ -41,14 +51,14 @@ ro = liftM fst rwOptions
 wo :: MonadReader DBContext m => m WriteOptions
 wo = liftM snd rwOptions
 
-put :: MonadReader DBContext IO => ByteString -> ByteString -> IO ()
+put :: MonadReader DBContext IO => DbValue -> DbValue -> IO ()
 put k v = do
-  db <- db
-  wo <- wo
-  Base.put db wo  k v
+  db' <- db
+  wo' <- wo
+  Base.put db' wo' (encode k) (encode v)
 
 get :: MonadReader DBContext IO => ByteString -> IO (Maybe ByteString)
 get k = do
-  db <- db
-  ro <- ro
-  Base.get db ro k
+  db' <- db
+  ro' <- ro
+  Base.get db' ro' k
