cb76b25c10de7ccf9252381c26f05f5380a79ce3
Alex Petrov <alexp@coffeenco.de>
2014-08-26 20:54:48 +0200

    Add gradual group by

--- src/Continuum/Actions.hs
+++ src/Continuum/Actions.hs
@@ -11,9 +11,18 @@ import Control.Applicative
 extractField :: String -> DbRecord -> DbValue
 extractField field (DbRecord _ record) = fromJust $ Map.lookup field record
 
-groupBy :: (Ord a) => [DbRecord] -> (DbRecord -> a) -> (Group a DbRecord)
+groupBy :: (Ord a) => [b] -> (b -> a) -> (Group a b)
 groupBy records groupFn = Group $ Map.toList $ foldr appendToGroup Map.empty records
                           where appendToGroup entry acc = Map.insertWith (++) (groupFn entry) [entry] acc
+
+
+gradualGroupBy :: (Ord a) => a -> Map.Map a Integer -> Map.Map a Integer
+gradualGroupBy i acc = Map.insertWith (+) i 1 acc
+
+
+-- groupBy :: (Ord a) => [DbRecord] -> (DbRecord -> a) -> (Group a DbRecord)
+-- groupBy records groupFn = Group $ Map.toList $ foldr appendToGroup Map.empty records
+--                           where appendToGroup entry acc = Map.insertWith (++) (groupFn entry) [entry] acc
 
 test = let records = [ makeRecord 123 [("a", (DbInt 1)), ("b", (DbString "1"))],
                        makeRecord 124 [("a", (DbInt 2)), ("b", (DbString "1"))],
