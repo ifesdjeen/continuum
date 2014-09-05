6ab5805d0db6e3d661187254f02ae35016fba130
Alex Petrov <alexp@coffeenco.de>
2014-09-05 22:48:37 +0200

    Cosmetics

--- src/Continuum/Options.hs
+++ src/Continuum/Options.hs
@@ -4,7 +4,7 @@ import Database.LevelDB
 
 opts :: Options
 opts = defaultOptions{ createIfMissing = True
-                     , cacheSize= 83886080
+                     , cacheSize= 512 * 1048576
                      -- , blockSize= 2048
                      -- , compression = NoCompression
                      -- , compression = NoCompressionNoCompression
