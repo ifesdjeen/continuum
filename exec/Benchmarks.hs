342d9e4674d5dc444aed55b9fc05f8857323d457
Alex Petrov <alexp@coffeenco.de>
2014-09-07 00:20:44 +0200

    Update benchmarks

--- exec/Benchmarks.hs
+++ exec/Benchmarks.hs
@@ -19,7 +19,9 @@ main :: IO ()
 main = defaultMain [
   -- bench "with pure value"   $ whnf (decode :: ByteString -> Either String ByteString) encoded
   bench "with pure value"   $ whnf (decode :: ByteString -> Either String DbValue)    encodedDbValue
-  , bench "with db record " $ whnf (decodeFieldByName "a" testSchema)               (indexingEncodeRecord testSchema testDbRecord 1)
+  , bench "with db record " $ whnf (decodeFieldByName "b" testSchema)               (indexingEncodeRecord testSchema testDbRecord 1)
+
+
   ]
 
 
