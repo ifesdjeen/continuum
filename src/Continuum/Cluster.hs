4d3e0a1af22c13f3b56d71d6f3a6c458ee761da1
Alex Petrov <alexp@coffeenco.de>
2014-09-19 22:00:50 +0200

    Draft implementation of cluster communication

--- src/Continuum/Cluster.hs
+++ src/Continuum/Cluster.hs
@@ -4,11 +4,14 @@
 
 module Continuum.Cluster where
 
+import           Control.Concurrent
+import           Control.Concurrent.STM
+import           Control.Monad
+import           Data.Serialize
+import qualified Data.Map as Map
+import qualified Data.Time.Clock.POSIX as Clock
 import           GHC.Generics           (Generic)
-import Data.Serialize
-import Control.Monad
-import Control.Concurrent
-import Control.Concurrent.STM
+import           System.Environment
 
 import qualified Control.Concurrent.Suspend.Lifted as Delay
 import qualified Control.Concurrent.Timer as Timer
@@ -21,119 +24,140 @@ import qualified Network.Socket as Socket
 import           Control.Monad.IO.Class (liftIO)
 import           Control.Monad (replicateM_)
 
-nLat :: Int -> Int -> String -> String -> IO ()
-nLat size count bindString connString = do
-      s1 <- N.socket N.Pair
-      _ <- N.bind s1 bindString
-      s2 <- N.socket N.Pair
-      _ <- N.connect s2 connString
-      -- let msg = C.pack $ replicate size 'a'
-      -- replicateM_ count (N.send s1 msg >> N.recv s2 >>= N.send s2 >> (liftIO . print) $ N.recv s1)
-      N.send s1 (C.pack $ replicate size 'a')
-      N.send s1 (C.pack $ replicate size 'b')
-      N.send s1 (C.pack $ replicate size 'c')
+data Node = Node String String
+          deriving(Generic, Show, Eq, Ord)
 
-      received <- N.recv s2
-      liftIO $ print received
-      received <- N.recv s2
-      liftIO $ print received
-      received <- N.recv s2
-      liftIO $ print received
+data NodeStatus = NodeStatus {lastHeartbeat :: Clock.POSIXTime}
+                deriving(Generic, Eq, Ord)
 
-      N.send s2 (C.pack $ replicate size 'a')
-      N.send s2 (C.pack $ replicate size 'b')
-      N.send s2 (C.pack $ replicate size 'c')
-
-      received <- N.recv s1
-      liftIO $ print received
-      received <- N.recv s1
-      liftIO $ print received
-      received <- N.recv s1
-      liftIO $ print received
-
-      N.close s1
-      N.close s2
-      return ()
-
-data Node = Node String Int
-          deriving(Generic, Show)
+type ClusterNodes = Map.Map Node NodeStatus
 
 instance Serialize Node
 
-data NodeContext = NodeContext { tcpServer       :: N.Socket N.Pair,
-                                 tcpClients      :: [N.Socket N.Pair]
+processRequest :: Node
+                  -> N.Socket N.Bus
+                  -> TVar ClusterNodes
+                  -> Request
+                  -> IO ()
+processRequest self socket shared (ImUp node) = do
+  nodes <- atomRead shared
+  N.send socket (encode $ NodeList (Map.keys nodes))
+  time <- Clock.getPOSIXTime
+  swap (\nodes -> Map.insert node (NodeStatus time) nodes) shared
+  return ()
 
-                               }
+processRequest self socket shared (Heartbeat node) = do
+  time <- Clock.getPOSIXTime
+  swap (\nodes -> Map.update (updateNodeTime time) node nodes) shared
+  -- print $ "got heartbeat from: " ++ (show node)
+  where updateNodeTime time nodeStatus = Just $ nodeStatus { lastHeartbeat = time }
 
-type NodeState a = StateT NodeContext IO a
+processRequest self socket shared (Introduction node) = do
+  time <- Clock.getPOSIXTime
+  swap (\nodes -> Map.insert node (NodeStatus time) nodes) shared
+  return ()
+
+processRequest self socket nodes (NodeList nodeList) = do
+  forM nodeList connectToCluster
+  return ()
+  where connectToCluster node = do
+          connectTo socket node self nodes
+          return ()
+
+connectTo :: N.Socket N.Bus
+             -> Node
+             -> Node
+             -> TVar ClusterNodes
+             -> IO ()
+connectTo socket other@(Node host port) self shared | other /= self = do
+  time <- Clock.getPOSIXTime
+  swap (\nodes -> Map.insert other (NodeStatus time) nodes) shared
+  N.connect socket ("tcp://" ++ host ++ ":" ++ port)
+  Timer.repeatedTimer introduce (Delay.msDelay 1000)
+  return ()
+  where introduce = N.send socket (encode $ Introduction self)
+
+connectTo socket other@(Node host port) self shared = return ()
+
+initializeNode :: N.Socket N.Bus
+                  -> Node
+                  -> Node
+                  -> TVar ClusterNodes
+                  -> IO ()
+initializeNode socket seed@(Node seedHost seedPort) self@(Node _ port) shared= do
+  _ <- N.bind socket ("tcp://*:" ++ port)
+  when (seedHost == "127.0.0.1") init
+  where init = do
+          N.connect socket ("tcp://" ++ seedHost ++ ":" ++ seedPort)
+          time <- Clock.getPOSIXTime
+          swap (\nodes -> Map.insert seed (NodeStatus time) nodes) shared
+          N.send socket (encode $ ImUp self)
+          return ()
 
 startSeedNode = do
   done <- newEmptyMVar
 
-  shared <- atomically $ newTVar ([] :: [Node])
+  [host, port, seedHost, seedPort] <- getArgs
+  shared <- atomically $ newTVar (Map.empty :: ClusterNodes)
 
+  serverSocket <- N.socket N.Bus
+  let seed = (Node seedHost seedPort)
+      self = (Node host port)
+      boundRequest = processRequest self serverSocket
+
+  initializeNode serverSocket seed self shared
   forkIO $ do
-    serverSocket <- N.socket N.Pair
-    _ <- N.bind serverSocket "tcp://*:5566"
     let receiveop = do
           received <- N.recv serverSocket
-          case (decode received :: Either String Node) of
-            (Left _) -> putMVar done ()
-            (Right a) -> appV (\i -> (i ++ [a])) shared
+          case (decode received :: Either String Request) of
+            (Left _)  -> putMVar done ()
+            (Right request) -> boundRequest shared request
           receiveop
     receiveop
 
   forkIO $ do
-    Timer.repeatedTimer (dispVar shared) (Delay.msDelay 5000)
+    Timer.repeatedTimer (printClusterStatus shared) (Delay.msDelay 1000)
+    Timer.repeatedTimer (sendHeartbeat serverSocket self) (Delay.msDelay 1000)
     return ()
 
   takeMVar done
 
   return ()
 
+sendHeartbeat :: N.Socket N.Bus -> Node -> IO ()
+sendHeartbeat socket node = N.send socket (encode $ Heartbeat node)
 
 -- Server Socket is used to recevie messages from all the nodes
 -- Server socket may be also used to broadcase messages to all the nodes
 -- Client socket is udes to push responses back messages from any other node (one at a time)
 
-startNormalNode = do
-
-  shared <- atomically $ newTVar ([] :: [Node])
-
-  -- forkIO $ do
-  --   serverSocket <- N.socket N.Pair
-  --   _ <- N.bind serverSocket "tcp://*:5577"
-  --   received <- N.recv serverSocket
-  --   print $ (decode received :: Either String Node)
-
-  forkIO $ do
-    clientSocket <- N.socket N.Pair
-    _ <- N.bind clientSocket "tcp://*:5567"
-    _ <- N.connect clientSocket "tcp://127.0.0.1:5566"
-    N.send clientSocket (encode $ Node "123" 555)
-    N.close clientSocket
-  return ()
-
-
--- main =
---   nLat 40 1000 "tcp://*:5566" "tcp://127.0.0.1:5566"
-
--- startNode = Socket.withSocketsDo $ do
---   bcClient <- Multicast.multicastReceiver "0.0.0.0" 4444
-
---   let loop = do
---         (msg, _, addr) <- Socket.recvFrom bcClient 1024
---         print (msg, addr)
---   timer1 <- Timer.repeatedTimer (loop) (Delay.msDelay 5000)
-
---   (bcServer, bcAddr) <- Multicast.multicastSender "127.0.0.1" 4444
---   timer <- Timer.repeatedTimer (Socket.sendTo bcServer "hier bin ich" bcAddr >> return ()) (Delay.msDelay 5000)
---   return ()
-
-
--- main =
---   nLat 40 1000 "tcp://*:5566" "tcp://127.0.0.1:5566"
-
 atomRead = atomically . readTVar
 dispVar x = atomRead x >>= print
-appV fn x = atomically $ readTVar x >>= writeTVar x . fn
+
+printClusterStatus :: TVar ClusterNodes -> IO ()
+printClusterStatus cluster = do
+  val <- atomRead cluster
+  time <- Clock.getPOSIXTime
+  print $ map (\(k,v) -> (k, showNodeStatus v time)) (Map.toList val)
+  print "------------"
+
+showNodeStatus :: NodeStatus -> Clock.POSIXTime -> String
+showNodeStatus nst currentTime =
+  if (lastHeartbeat nst) >= (currentTime - nodeTimeout)
+  then "active"
+  else "down"
+
+swap fn x = atomically $ readTVar x >>= writeTVar x . fn
+
+--- Protocol Specofication
+
+data Request = ImUp Node
+             | Introduction Node
+             | NodeList [Node]
+             | Heartbeat Node
+             deriving(Generic, Show)
+
+instance Serialize Request
+
+nodeTimeout :: Clock.POSIXTime
+nodeTimeout = 5
