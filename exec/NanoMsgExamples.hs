bc3a1803802e541d7517c2533d6b4a68ac7a9ba3
Alex Petrov <alexp@coffeenco.de>
2014-09-19 22:01:45 +0200

    Add nanomessage examples

---
+++ exec/NanoMsgExamples.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Continuum.Cluster
+
+main :: IO ()
+main = startSeedNode
+
+-- main =
+--   nLat 40 1000 "tcp://*:5566" "tcp://127.0.0.1:5566"
