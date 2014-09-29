7d85f073ab9ce4453d114bdfac8b9de220ebc1a4
Alex Petrov <alexp@coffeenco.de>
2014-09-29 23:53:15 +0200

    Start prototyping a context

---
+++ src/Continuum/Context.hs
@@ -0,0 +1,17 @@
+module Continuum.Context ()
+       where
+
+import qualified Continuum.Types as Types
+
+-- maybeLoadContext
+-- flushContext
+
+-- addField
+
+-- | Removing field is a very costly operation
+-- removeField
+
+-- systemDb
+--
+
+-- Sequence number should be issued by the "leader"
