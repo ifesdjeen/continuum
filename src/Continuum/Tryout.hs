51cdbfe4d88eb36575a125f2cd4025e4bef46cb3
Alex Petrov <alexp@coffeenco.de>
2014-09-30 19:37:46 +0200

    Fix tryout

--- src/Continuum/Tryout.hs
+++ src/Continuum/Tryout.hs
@@ -4,7 +4,7 @@
 module Continuum.Tryout where
 
 import           Control.Monad.State
-
+import           Data.ByteString (ByteString)
 import           Continuum.Storage
 import           Continuum.Serialization
 import           Continuum.Types
@@ -12,32 +12,27 @@ import           Continuum.Types
 import           Continuum.Folds
 -- import qualified Data.Map as Map
 
-testDBPath :: String
-testDBPath = "/tmp/haskell-leveldb-tests5"
+testDbPath :: String
+testDbPath = "/tmp/haskell-leveldb-tests5"
+
+testDbName :: ByteString
+testDbName = "testdb"
 
 testSchema :: DbSchema
 testSchema = makeSchema [ ("a", DbtInt)
                         , ("b", DbtString)]
 
-main :: IO ()
-main = runApp testDBPath testSchema $ do
+main :: IO (Either DbError [DbResult])
+main = runApp testDbPath testSchema $ do
 
-  putRecord $ makeRecord 123 [("a", (DbInt 1)), ("b", (DbString "a"))]
-  putRecord $ makeRecord 128 [("a", (DbInt 2)), ("b", (DbString "a"))]
-  -- putRecord $ makeRecord 129 [("a", (DbInt 3)), ("b", (DbString "b"))]
-  -- putRecord $ makeRecord 130 [("a", (DbInt 4)), ("b", (DbString "d"))]
+  createDatabase testDbName testSchema
 
-  -- a <- scan (Just $ encodeBeginTimestamp 126) (withFullRecord id alwaysTrue append) []
+  putRecord testDbName $ makeRecord 123 [("a", (DbInt 1)), ("b", (DbString "a"))]
+  putRecord testDbName $ makeRecord 128 [("a", (DbInt 2)), ("b", (DbString "a"))]
 
-  -- a <- aggregateAllByField "b" snd (groupReduce id id (\_ acc -> acc + 1) 1) Map.empty
-  -- a <- aggregateAllByField "a" snd (\i acc -> acc + (unpackInt i)) 0
+  a <- scan testDbName (TsSingleKey 123)    Record appendFold
+  a <- scan testDbName (TsOpenEnd 123)      Record appendFold
+  a <- scan testDbName (TsKeyRange 123 300) Record appendFold
 
-  -- a <- aggregateAllByField "b" appendFold
-  -- a <- findByTimestamp 123
-  -- liftIO $ putStrLn $ show a
-
-  a <- scan (TsSingleKey 123)    Record appendFold
-  a <- scan (TsOpenEnd 123)      Record appendFold
-  a <- scan (TsKeyRange 123 300) Record appendFold
-  liftIO $ print (show a)
+  return a
   -- a <- scanAll2 id (:) []
