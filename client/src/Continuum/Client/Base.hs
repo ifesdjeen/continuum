308660fb21560287a38506929788f7ca9b29a236
Alex Petrov <alexp@coffeenco.de>
2014-11-17 21:01:05 +0100

    Switch to client req/rep sockets

--- client/src/Continuum/Client/Base.hs
+++ client/src/Continuum/Client/Base.hs
@@ -13,23 +13,40 @@ import           Data.Serialize (encode, decode)
 import qualified Continuum.Common.Serialization as S
 import           Continuum.Common.Types
 
-data ContinuumClient = ContinuumClient { clientSocket :: N.Socket N.Bus }
+data ContinuumClient = ContinuumClient
+    { clientSocket   :: N.Socket N.Req
+    , clientEndpoint :: N.Endpoint }
 
 connect :: String
            -> String
            -> IO ContinuumClient
 
 connect host port = do
-  socket <- N.socket N.Bus
-  _      <- N.connect socket ("tcp://" ++ host ++ ":" ++ port)
-  return ContinuumClient { clientSocket = socket }
+  socket   <- N.socket N.Req
+  endpoint <- N.connect socket ("tcp://" ++ host ++ ":" ++ port)
+  return ContinuumClient { clientSocket   = socket
+                         , clientEndpoint = endpoint}
+
+disconnect :: ContinuumClient -> IO ()
+disconnect client = do
+  N.shutdown (clientSocket client) (clientEndpoint client)
+  N.close (clientSocket client)
+  return ()
 
 executeQuery :: ContinuumClient
              -> Query
              -> IO (DbErrorMonad DbResult)
 executeQuery client query = do
   let sock = (clientSocket client)
-  -- It's a request query
-  _      <-  N.send sock (encode (Query query))
+  _      <-  N.send sock (encode (RunQuery query))
+  bs     <-  N.recv sock
+  return $ (S.decodeDbResult bs)
+
+sendRequest :: ContinuumClient
+             -> Request
+             -> IO (DbErrorMonad DbResult)
+sendRequest client request = do
+  let sock = (clientSocket client)
+  _      <-  N.send sock (encode request)
   bs     <-  N.recv sock
   return $ (S.decodeDbResult bs)
