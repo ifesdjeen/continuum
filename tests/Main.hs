b4fc78bc4a95541f9fedc31ddb7f56fd90762d8a
Alex Petrov <alexp@coffeenco.de>
2014-11-27 21:31:53 +0100

    Remove serialization tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -3,12 +3,10 @@
 
 module Main where
 
-import qualified Serialization.Test as Serialization
 import qualified Storage.Test       as Storage
 import qualified Cluster.Test       as Cluster
 
 main :: IO ()
 main = do
   Storage.main
-  Serialization.main
   Cluster.main
