60b0d682f86a5bc765beb418975c3012ea84f8c6
Alex Petrov <oleksandr.petrov@gmail.com>
2014-12-10 22:12:50 +0100

    Implement a couple more instances

--- src/Continuum/Http/Encoding.hs
+++ src/Continuum/Http/Encoding.hs
@@ -11,21 +11,23 @@ import           Data.ByteString             ( ByteString )
 import           Data.ByteString.Lazy.Char8  ( unpack )
 import           Data.Text                   ( pack )
 import           Data.Text.Encoding          ( decodeUtf8 )
-import           Continuum.Types
+import           Continuum.Common.Types
 
 instance ToJSON DbError where
   toJSON _ = "Error Occured"
 
-instance ToJSON (Map.Map ByteString (DbSchema, a)) where
-  toJSON dbsMap = object $
-                  concat $
-                  map (\(k, v) -> [(decodeUtf8 k) .= (fst v)]) (Map.toList dbsMap)
+instance ToJSON ByteString where
+  toJSON = toJSON . decodeUtf8
+
+instance (ToJSON a, ToJSON b) => ToJSON (Map.Map a b) where
+  toJSON m = object $
+             concat $
+             map (\(k, v) -> [(pack $ show $ toJSON k) .= (toJSON v)]) (Map.toList m)
 
 instance ToJSON DbSchema where
-  toJSON DbSchema{..} = object $
-                        concat $
-                        map (\(k, v) -> [(decodeUtf8 k) .= (show v)]) (Map.toList schemaMappings)
+  toJSON DbSchema{..} = toJSON $ schemaMappings
 
+instance ToJSON DbType
 
 instance ToJSON DbValue where
   toJSON (DbInt v)    = toJSON v
