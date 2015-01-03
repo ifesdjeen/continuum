66db366a10270a090927ed9a73a243cb833dc034
Alex Petrov <oleksandr.petrov@gmail.com>
2015-01-03 20:35:45 +0100

    Cleanup

--- exec/ShellClient.hs
+++ exec/ShellClient.hs
@@ -3,13 +3,13 @@
 module Main where
 
 
-import qualified Data.Map               as Map
+-- import qualified Data.Map               as Map
 
-import           Data.Aeson
-import           Data.ByteString.Lazy.Char8  ( unpack )
-import           Data.Text                   ( pack )
-import           Data.Text.Encoding          ( decodeUtf8 )
-import           Continuum.Client.Base       ( DbRecord(..), DbResult(..), SelectQuery(..), Request(..), DbValue(..), connect, sendRequest )
+-- import           Data.Aeson
+-- import           Data.ByteString.Lazy.Char8  ( unpack )
+-- import           Data.Text                   ( pack )
+-- import           Data.Text.Encoding          ( decodeUtf8 )
+-- import           Continuum.Client.Base       ( DbRecord(..), DbResult(..), SelectQuery(..), Request(..), DbValue(..), connect, sendRequest )
 
 -- instance Continuum.DbValue ToJSON where
 
