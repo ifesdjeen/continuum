52a98d7aff5d81e130188f5ab079b05dc4964302
Alex Petrov <oleksandr.petrov@gmail.com>
2015-01-09 23:10:36 +0100

    Simplify the fetches, avoid storing intermediate data anywhere

--- cbits/continuum.c
+++ cbits/continuum.c
@@ -6,8 +6,6 @@
 #include <stdint.h>
 #include <string.h>
 
-#define RESULTSET_BASE 100
-
 int
 bitwise_compare(void* shared,
                 const char* a, size_t alen,
@@ -17,38 +15,30 @@ bitwise_compare(void* shared,
   return r;
 }
 
-db_results_t*
-scan_entire_keyspace(leveldb_t* db,
-                     leveldb_readoptions_t* roptions) {
-  key_value_pair_t *kvps = (key_value_pair_t *)malloc(RESULTSET_BASE * sizeof(key_value_pair_t));
+void
+scan_entire_keyspace(leveldb_t*             db,
+                     leveldb_readoptions_t* roptions,
+                     void (*append)(const char* key, size_t key_len,
+                                    const char* val, size_t val_len)) {
+  leveldb_iterator_t* iter = leveldb_create_iterator(db, roptions);
+  leveldb_iter_seek_to_first(iter);
 
-  int count = 0;
-  {
-    leveldb_iterator_t* iter = leveldb_create_iterator(db, roptions);
-    leveldb_iter_seek_to_first(iter);
+  while(leveldb_iter_valid(iter)) {
+    size_t current_key_len;
+    const char* current_key = leveldb_iter_key(iter, &current_key_len);
 
-    while(leveldb_iter_valid(iter)) {
-      kvps[count].key = leveldb_iter_key(iter, &kvps[count].key_len);
-      kvps[count].val = leveldb_iter_value(iter, &kvps[count].val_len);
+    size_t current_val_len;
+    const char* current_val = leveldb_iter_value(iter, &current_val_len);
 
-      count += 1;
-      if(count % RESULTSET_BASE == 0) {
-        kvps = (key_value_pair_t *)realloc(kvps, (count + RESULTSET_BASE) * sizeof(key_value_pair_t));
-      }
-      leveldb_iter_next(iter);
-    }
-
-    leveldb_iter_destroy(iter);
+    append(current_key, current_key_len, current_val, current_val_len);
+    leveldb_iter_next(iter);
   }
 
-  db_results_t* db_result = calloc(1, sizeof(db_results_t));
-  db_result->results      = kvps;
-  db_result->count        = count;
-
-  return db_result;
+  leveldb_iter_destroy(iter);
+  fflush(stdout);
 }
 
-db_results_t*
+void
 scan_range(leveldb_t*             db,
            leveldb_readoptions_t* roptions,
            const char*            start_at,
@@ -57,10 +47,38 @@ scan_range(leveldb_t*             db,
            size_t                 end_at_len,
            int (*compare)(void*,
                           const char* a, size_t alen,
-                          const char* b, size_t blen)) {
-  key_value_pair_t *kvps = (key_value_pair_t *)malloc(RESULTSET_BASE * sizeof(key_value_pair_t));
+                          const char* b, size_t blen),
+           void (*append)(const char* key, size_t key_len,
+                         const char* val, size_t val_len)) {
+  leveldb_iterator_t* iter = leveldb_create_iterator(db, roptions);
+  leveldb_iter_seek(iter, start_at, start_at_len);
 
-  int count = 0;
+  while(leveldb_iter_valid(iter)) {
+    size_t current_key_len;
+    const char* current_key = leveldb_iter_key(iter, &current_key_len);
+
+    if(bitwise_compare(NULL, current_key, current_key_len, end_at, end_at_len) > 0) {
+      break;
+    }
+
+    size_t current_val_len;
+    const char* current_val = leveldb_iter_value(iter, &current_val_len);
+
+    append(current_key, current_key_len, current_val, current_val_len);
+
+    leveldb_iter_next(iter);
+  }
+
+  leveldb_iter_destroy(iter);
+}
+
+void
+scan_open_end(leveldb_t*             db,
+              leveldb_readoptions_t* roptions,
+              const char*            start_at,
+              size_t                 start_at_len,
+              void (*append)(const char* key, size_t key_len,
+                             const char* val, size_t val_len)) {
   {
     leveldb_iterator_t* iter = leveldb_create_iterator(db, roptions);
     leveldb_iter_seek(iter, start_at, start_at_len);
@@ -69,70 +87,13 @@ scan_range(leveldb_t*             db,
       size_t current_key_len;
       const char* current_key = leveldb_iter_key(iter, &current_key_len);
 
-      if(bitwise_compare(NULL, current_key, current_key_len, end_at, end_at_len) > 0) {
-        break;
-      }
+      size_t current_val_len;
+      const char* current_val = leveldb_iter_value(iter, &current_val_len);
 
-      kvps[count].key     = current_key;
-      kvps[count].key_len = current_key_len;
-      kvps[count].val     = leveldb_iter_value(iter, &kvps[count].val_len);
-      count += 1;
-
-      if(count % RESULTSET_BASE == 0) {
-        kvps = (key_value_pair_t *)realloc(kvps, (count + RESULTSET_BASE) * sizeof(key_value_pair_t));
-      }
+      append(current_key, current_key_len, current_val, current_val_len);
       leveldb_iter_next(iter);
     }
 
     leveldb_iter_destroy(iter);
   }
-
-  db_results_t* db_result = calloc(1, sizeof(db_results_t));
-  db_result->results = kvps;
-  db_result->count = count;
-
-  return db_result;
-}
-
-db_results_t*
-scan_open_end(leveldb_t*             db,
-              leveldb_readoptions_t* roptions,
-              const char*            start_at,
-              size_t                 start_at_len) {
-  key_value_pair_t *kvps = (key_value_pair_t *)malloc(RESULTSET_BASE * sizeof(key_value_pair_t));
-
-  int count = 0;
-  {
-    leveldb_iterator_t* iter = leveldb_create_iterator(db, roptions);
-    leveldb_iter_seek(iter, start_at, start_at_len);
-
-    while(leveldb_iter_valid(iter)) {
-      kvps[count].key = leveldb_iter_key(iter, &kvps[count].key_len);
-      kvps[count].val = leveldb_iter_value(iter, &kvps[count].val_len);
-
-      count += 1;
-      if(count % RESULTSET_BASE == 0) {
-        kvps = (key_value_pair_t *)realloc(kvps, (count + RESULTSET_BASE) * sizeof(key_value_pair_t));
-      }
-      leveldb_iter_next(iter);
-    }
-
-    leveldb_iter_destroy(iter);
-  }
-
-  db_results_t* db_result = calloc(1, sizeof(db_results_t));
-  db_result->results      = kvps;
-  db_result->count        = count;
-
-  return db_result;
-}
-
-
-void
-free_db_results(db_results_t* dbr) {
-  if (dbr == NULL)
-    return;
-
-  free(dbr->results);
-  free(dbr);
 }
--- cbits/continuum.h
+++ cbits/continuum.h
@@ -6,30 +6,13 @@
 
 #include "leveldb/c.h"
 
-struct key_value_pair_s
-{
-  const char* key;
-  size_t key_len;
-  const char* val;
-  size_t val_len;
-};
+void
+scan_entire_keyspace(leveldb_t*             db,
+                     leveldb_readoptions_t* roptions,
+                     void (*append)(const char* key, size_t key_len,
+                                    const char* val, size_t val_len));
 
-typedef struct key_value_pair_s key_value_pair_t;
-
-
-struct db_results_s
-{
-  key_value_pair_t* results;
-  int               count;   /* TODO: Maybe unsigned? */
-};
-
-typedef struct db_results_s db_results_t;
-
-db_results_t*
-scan_entire_keyspace(leveldb_t* db,
-                     leveldb_readoptions_t* roptions);
-
-db_results_t*
+void
 scan_range(leveldb_t*             db,
            leveldb_readoptions_t* roptions,
            const char*            start_at,
@@ -38,16 +21,17 @@ scan_range(leveldb_t*             db,
            size_t                 end_at_len,
            int (*compare)(void*,
                           const char* a, size_t alen,
-                          const char* b, size_t blen));
+                          const char* b, size_t blen),
+           void (*append)(const char* key, size_t key_len,
+                          const char* val, size_t val_len));
 
-db_results_t*
+void
 scan_open_end(leveldb_t*             db,
               leveldb_readoptions_t* roptions,
               const char*            start_at,
-              size_t                 start_at_len);
-
-void
-free_db_results(db_results_t* ptr);
+              size_t                 start_at_len,
+              void (*append)(const char* key, size_t key_len,
+                            const char* val, size_t val_len));
 
 int
 bitwise_compare(void* shared,
