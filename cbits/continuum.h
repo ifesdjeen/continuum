0a86786f16812f4a838722187331ed5869135160
Alex Petrov <oleksandr.petrov@gmail.com>
2015-01-26 20:18:50 +0100

    Cleanup unused c functions

--- cbits/continuum.h
+++ cbits/continuum.h
@@ -6,49 +6,15 @@
 
 #include "leveldb/c.h"
 
-void
-scan_entire_keyspace(leveldb_t*             db,
-                     leveldb_readoptions_t* roptions,
-                     void (*append)(const char* key, size_t key_len,
-                                    const char* val, size_t val_len));
-
-void
-scan_range(leveldb_t*             db,
-           leveldb_readoptions_t* roptions,
-           const char*            start_at,
-           size_t                 start_at_len,
-           int (*compare)(const char* a, size_t alen),
-           void (*append)(const char* key, size_t key_len,
-                          const char* val, size_t val_len));
-
-void
-scan_range_butfirst(leveldb_t*             db,
-                    leveldb_readoptions_t* roptions,
-                    const char*            start_at,
-                    size_t                 start_at_len,
-                    int (*compare)(const char* a, size_t alen),
-                    void (*append)(const char* key, size_t key_len,
-                                   const char* val, size_t val_len));
-void
-scan_open_end(leveldb_t*             db,
-              leveldb_readoptions_t* roptions,
-              const char*            start_at,
-              size_t                 start_at_len,
-              void (*append)(const char* key, size_t key_len,
-                            const char* val, size_t val_len));
-
-void
-scan_open_end_butfirst(leveldb_t*             db,
-                       leveldb_readoptions_t* roptions,
-                       const char*            start_at,
-                       size_t                 start_at_len,
-                       void (*append)(const char* key, size_t key_len,
-                                      const char* val, size_t val_len));
-
 int
 bitwise_compare(void* shared,
                 const char* a, size_t alen,
                 const char* b, size_t blen);
+
+int
+constantly_true(const char* b, size_t blen) {
+  return 0;
+}
 
 void
 scan(leveldb_t*             db,
@@ -59,4 +25,7 @@ scan(leveldb_t*             db,
      void (*append)(const char* key, size_t key_len,
                     const char* val, size_t val_len),
      int                    skip_first);
+
+
+
 #endif /* CONTINUUM_H */
