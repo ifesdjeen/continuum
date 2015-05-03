f8fe721fd86ce79767a28d6e40d0d9c0b61e95e6
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-04 00:14:16 +0200

    Rename test to spec, improve test itself

--- tests/Continuum/Storage/RecordStorageSpec.hs
+++ tests/Continuum/Storage/RecordStorageSpec.hs
@@ -1,20 +1,23 @@
-module Continuum.Storage.RecordStorageTest () where
+{-# LANGUAGE OverloadedStrings #-}
 
+module Continuum.Storage.RecordStorageSpec where
+
+import Continuum.Storage.RecordStorage
 import           Control.Applicative        hiding (empty)
 import           Control.Monad.Catch
-import           Control.Monad.Identity
-import           Control.Monad.IO.Class
 import qualified Data.ByteString            as BS
 import           Data.ByteString.Char8      (ByteString, singleton, unpack)
-import           Data.Default
 import           Data.Foldable              (foldMap)
 import           Data.List
 import           Data.Monoid
 import           Database.LevelDB.Base
 import           Database.LevelDB.Internal  (unsafeClose)
-import qualified Database.LevelDB.Streaming as S
 import           System.Directory
 import           System.IO.Temp
+import           Data.Default
+import Test.Hspec
+
+import Debug.Trace
 
 data Rs = Rs DB FilePath
 
@@ -23,15 +26,22 @@ spec = do
 
   describe "Primitive" $ do
     it "passes Word8  Round Trip" $ do
-      1 == 1
+      r <- bracket initDB destroyDB $ \(Rs db _) -> do
+        write db def [ Put "a" "one"
+                     , Put "b" "two"
+                     , Put "c" "three"]
+        res <- withIter db def (\iter -> do
+          toList $ entrySlice iter (KeyRange {start = "a", end = (\i -> i `compare` "c")}) Asc (\x -> Right x))
+        return res
+      r `shouldBe` (Right [("a","one"),("b","two"),("c","three")])
   where
     initDB = do
         tmp <- getTemporaryDirectory
         dir <- createTempDirectory tmp "leveldb-streaming-tests"
         db  <- open dir defaultOptions { createIfMissing = True }
-        write db def
-          . map ( \ c -> let c' = singleton c in Put c' c')
-          $ ['A'..'Z']
+        -- write db def
+        --   . map ( \ c -> let c' = singleton c in Put c' c')
+        --   $ ['A'..'Z']
         return $ Rs db dir
 
     destroyDB (Rs db dir) = unsafeClose db `finally` destroy dir defaultOptions
