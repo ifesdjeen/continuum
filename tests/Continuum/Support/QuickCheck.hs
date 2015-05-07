c3f5aca7df4ae9aaa17500c3ae7c2504d59eea90
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-07 23:22:30 +0200

    Add more generators

---
+++ tests/Continuum/Support/QuickCheck.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans            #-}
+
+module Continuum.Support.QuickCheck where
+
+import Data.ByteString       ( ByteString )
+import Data.ByteString.Char8 ( pack )
+import Continuum.Types
+import Test.QuickCheck
+import Continuum.Serialization.Schema ( makeSchema, validate )
+import Continuum.Serialization.Record ( makeRecord )
+instance Arbitrary DbType where
+  arbitrary = elements [ DbtLong, DbtInt, DbtByte, DbtShort, DbtFloat, DbtDouble, DbtString]
+
+data SchemaTestRow = TestRow ByteString DbType DbValue
+                   deriving (Show)
+
+instance Arbitrary ByteString where
+  arbitrary = pack <$> suchThat arbitrary (\str -> and [not $ any (\x -> '\NUL' == x) str,
+                                                        not (length str == 0) ])
+instance Arbitrary SchemaTestRow where
+  arbitrary = do
+    name <- arbitrary
+    tp <- arbitrary
+    val <- case tp of
+            DbtLong   -> DbLong   <$> suchThat arbitrary (\i -> i > 0)
+            DbtInt    -> DbInt    <$> suchThat arbitrary (\i -> i > 0)
+            DbtByte   -> DbByte   <$> suchThat arbitrary (\i -> i > 0)
+            DbtShort  -> DbShort  <$> suchThat arbitrary (\i -> i > 0)
+            DbtFloat  -> DbFloat  <$> suchThat arbitrary (\i -> i > 0)
+            DbtDouble -> DbDouble <$> suchThat arbitrary (\i -> i > 0)
+            DbtString -> DbString <$> arbitrary
+    return $ TestRow name tp val
+
+instance Arbitrary DbValue where
+  arbitrary = do
+    oneof [DbLong    <$> arbitrary
+          , DbInt    <$> arbitrary
+          , DbByte   <$> arbitrary
+          , DbShort  <$> arbitrary
+          , DbFloat  <$> arbitrary
+          , DbDouble <$> arbitrary
+          , DbString <$> arbitrary]
+
+
+instance Arbitrary DbSchema where
+  arbitrary = arbitrary >>= return . makeSchema
+
+instance Arbitrary DbRecord where
+  arbitrary = do
+    timestamp <- arbitrary
+    vals <- arbitrary
+    return $ makeRecord timestamp vals
+
+instance Arbitrary (DbSchema, [DbRecord]) where
+  arbitrary = do
+    schema <- arbitrary
+    records <- listOf $ suchThat arbitrary (validate schema)
+    return (schema, records)
