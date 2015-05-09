3e0a373a8aa8cbfd7c7757c2d470a5d4e30d6764
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-09 14:09:58 +0200

    Cleanup storage test

--- tests/Continuum/Storage/RecordStorageSpec.hs
+++ tests/Continuum/Storage/RecordStorageSpec.hs
@@ -2,24 +2,26 @@
 
 module Continuum.Storage.RecordStorageSpec where
 
+import Continuum.Types
 import Continuum.Serialization.Record
 import Continuum.Serialization.Schema ( makeSchema )
-import qualified Continuum.Storage.RecordStorage as Storage
-import Continuum.Types
-import           Control.Monad.Catch
-import Database.LevelDB.Base ( BatchOp(..), DB, withIter, write, open, defaultOptions, createIfMissing, destroy )
-import           Database.LevelDB.Internal  (unsafeClose)
-import           Data.ByteString                   ( ByteString )
-import           System.Directory
-import           System.IO.Temp
-import           Data.Default
-import Test.Hspec
-
 import Continuum.Support.QuickCheck
+import qualified Continuum.Storage.RecordStorage as Storage
+
+import Control.Monad.Catch
+import System.Directory
+import System.IO.Temp
+
+
+import Test.Hspec
 import Test.QuickCheck.Monadic
-import Test.QuickCheck.Property ( Property(..) )
 import Test.QuickCheck
-import GHC.Exts ( sortWith )
+
+import Data.Default
+import Data.ByteString           ( ByteString )
+import Database.LevelDB.Internal (unsafeClose)
+import Database.LevelDB.Base     ( BatchOp(..), DB, withIter, write, open, defaultOptions, createIfMissing, destroy )
+import GHC.Exts                  ( sortWith )
 
 -- import Debug.Trace
 
@@ -27,18 +29,6 @@ data Rs = Rs DB FilePath
 
 tupleToBatchOp :: (ByteString, ByteString) -> BatchOp
 tupleToBatchOp (x,y) = Put x y
-
-prop_RoundTrip :: (DbSchema, [DbRecord]) -> Property
-prop_RoundTrip (schema, records) = monadicIO $ do
-  r   <- run $ bracket initDB destroyDB $ \(Rs db _) -> do
-    _   <- write db def (map (tupleToBatchOp . (encodeRecord schema 1)) records)
-    res <- withIter db def (\iter -> do
-                               Storage.toList $ Storage.entrySlice iter AllKeys Asc (decodeRecord Record schema))
-    return res
-  assert $ (sortWith ts <$> r) == Right (sortWith ts records)
-
-  where ts (DbRecord i _) = i
-
 
 spec :: Spec
 spec = do
@@ -67,20 +57,34 @@ spec = do
         return res
       r `shouldBe` (Right records)
 
---    it "asd" $ do
+    it "asd" $ do
+      let schema  = makeSchema (zip ["a", "b", "c", "d"] [DbtShort, DbtShort, DbtDouble, DbtShort])
+          records = [ makeRecord 1 [("c", DbDouble 9.93), ("b", DbShort 2), ("a", DbShort 2), ("d", DbShort 3)]
+                    , makeRecord 2 [("c", DbDouble 2.27), ("b", DbShort 3), ("a", DbShort 4), ("d", DbShort 3)]
+                    , makeRecord 4 [("c", DbDouble 7.14), ("b", DbShort 4), ("a", DbShort 3), ("d", DbShort 3)]]
+      r   <- bracket initDB destroyDB $ \(Rs db _) -> do
+        _   <- write db def (map (tupleToBatchOp . (encodeRecord schema 1)) records)
+        res <- withIter db def (\iter -> do
+                                   Storage.toList $ Storage.entrySlice iter AllKeys Asc (decodeRecord Record schema))
+        return res
+      (sortWith ts <$> r) `shouldBe` Right (sortWith ts records)
 
-      -- let schema  = makeSchema (zip ["\184","mJs","\ENQ;'","\222S\DLEf","\ENQ\129\137\DC3"] [DbtShort,DbtShort,DbtDouble,DbtShort,DbtFloat])
-      --     records = [makeRecord 1 ([("\ENQ;'",DbDouble 9.931976876439089),("\ENQ\129\137\DC3",DbFloat 14.999481),("mJs",DbShort 2),("\184",DbShort 2),("\222S\DLEf",DbShort 3)]),makeRecord 2 ([("\ENQ;'",DbDouble 2.277007862297778),("\ENQ\129\137\DC3",DbFloat 0.98638123),("mJs",DbShort 2),("\184",DbShort 4),("\222S\DLEf",DbShort 3)]),makeRecord 4 ([("\ENQ;'",DbDouble 7.144124519534123e-4),("\ENQ\129\137\DC3",DbFloat 2.1302373),("mJs",DbShort 2),("\184",DbShort 3),("\222S\DLEf",DbShort 3)]),makeRecord 1 ([("\ENQ;'",DbDouble 3.9215858195166775),("\ENQ\129\137\DC3",DbFloat 7.664713),("mJs",DbShort 1),("\184",DbShort 3),("\222S\DLEf",DbShort 5)])]
-      -- r   <- bracket initDB destroyDB $ \(Rs db _) -> do
-      --   _   <- write db def (map (tupleToBatchOp . (encodeRecord schema 1)) records)
-      --   res <- withIter db def (\iter -> do
-      --                              Storage.toList $ Storage.entrySlice iter AllKeys Asc (decodeRecord Record schema))
-      --   return res
-      -- (sortWith ts <$> r) `shouldBe` Right (sortWith ts records)
-      -- where ts (DbRecord i _) = i
 
     it "Can iterate over the decoded records" $ do
       property $ prop_RoundTrip
+
+-- |
+-- | Properties
+-- |
+
+prop_RoundTrip :: (DbSchema, [DbRecord]) -> Property
+prop_RoundTrip (schema, records) = monadicIO $ do
+  r   <- run $ bracket initDB destroyDB $ \(Rs db _) -> do
+    _   <- write db def (map (tupleToBatchOp . (encodeRecord schema 1)) records)
+    res <- withIter db def (\iter -> do
+                               Storage.toList $ Storage.entrySlice iter AllKeys Asc (decodeRecord Record schema))
+    return res
+  assert $ (sortWith ts <$> r) == Right (sortWith ts records)
 
 
 initDB :: IO Rs
@@ -92,3 +96,6 @@ initDB = do
 
 destroyDB :: Rs -> IO ()
 destroyDB (Rs db dir) = unsafeClose db `finally` destroy dir defaultOptions
+
+ts :: DbRecord -> Integer
+ts (DbRecord i _) = i
