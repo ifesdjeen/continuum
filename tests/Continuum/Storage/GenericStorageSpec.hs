bddc37b520d3b8e8db3dd977eebe545155203456
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-14 23:38:44 +0200

    Simplify code

--- tests/Continuum/Storage/GenericStorageSpec.hs
+++ tests/Continuum/Storage/GenericStorageSpec.hs
@@ -6,41 +6,29 @@ import Continuum.Types
 import Continuum.Serialization.Record
 import Continuum.Serialization.Schema ( makeSchema )
 import Continuum.Support.QuickCheck
+import Continuum.Support.TmpDb
 import Continuum.Storage.GenericStorage
-import qualified Continuum.Stream as SM
-
-import Control.Monad.Catch
-import System.Directory
-import System.IO.Temp
+import qualified Continuum.Stream as S
 
 import Data.Default
 import GHC.Exts                  ( sortWith )
-import Database.LevelDB.Internal ( unsafeClose )
-import Database.LevelDB.Base     ( BatchOp(..), DB, withIter, write, open, defaultOptions, createIfMissing, destroy )
-
+import Database.LevelDB.Base     ( withIter, write )
+import Control.Exception         ( bracket )
 import Test.Hspec
 import Test.QuickCheck.Monadic
 import Test.QuickCheck
-
-
--- import Debug.Trace
-
-data Rs = Rs DB FilePath
-
-tupleToBatchOp :: Entry -> BatchOp
-tupleToBatchOp (x,y) = Put x y
 
 spec :: Spec
 spec = do
 
   describe "Recorg Storage" $ do
     it "Can iterate over the items and append them to the array" $ do
-      r <- bracket initDB destroyDB $ \(Rs db _) -> do
-        write db def [ Put "a" "one"
-                     , Put "b" "two"
-                     , Put "c" "three"]
+      r <- withTmpDb $ \(Rs db _) -> do
+        populate db [ Put "a" "one"
+                    , Put "b" "two"
+                    , Put "c" "three"]
         res <- withIter db def (\iter -> do
-                                   SM.toList $ entrySlice iter AllKeys Asc)
+                                   S.toList $ entrySlice iter AllKeys Asc)
         return res
       r `shouldBe` [("a","one"),("b","two"),("c","three")]
 
@@ -48,13 +36,12 @@ spec = do
       let schema  = makeSchema [ ("a", DbtLong) ]
           records = [ makeRecord 1 [ ("a", DbLong 1) ]
                     , makeRecord 2 [ ("a", DbLong 2) ]]
-      r <- bracket initDB destroyDB $ \(Rs db _) -> do
-
-        write db def (map (tupleToBatchOp . (encodeRecord schema 1)) records)
+      r <- withTmpDb $ \(Rs db _) -> do
+        populate db (map (tupleToBatchOp . (encodeRecord schema 1)) records)
 
         res <- withIter db def (\iter ->
-                                 SM.collect
-                                 $ SM.toList
+                                 S.collect
+                                 $ S.toList
                                  $ withDecoded Record schema
                                  $ entrySlice iter AllKeys Asc)
         return res
@@ -65,11 +52,11 @@ spec = do
           records = [ makeRecord 1 [("c", DbDouble 9.93), ("b", DbShort 2), ("a", DbShort 2), ("d", DbShort 3)]
                     , makeRecord 2 [("c", DbDouble 2.27), ("b", DbShort 3), ("a", DbShort 4), ("d", DbShort 3)]
                     , makeRecord 4 [("c", DbDouble 7.14), ("b", DbShort 4), ("a", DbShort 3), ("d", DbShort 3)]]
-      r   <- bracket initDB destroyDB $ \(Rs db _) -> do
-        _   <- write db def (map (tupleToBatchOp . (encodeRecord schema 1)) records)
+      r   <- withTmpDb $ \(Rs db _) -> do
+        _   <- populate db (map (tupleToBatchOp . (encodeRecord schema 1)) records)
         res <- withIter db def (\iter -> do
-                                   SM.collect
-                                   $ SM.toList
+                                   S.collect
+                                   $ S.toList
                                    $ withDecoded Record schema
                                    $ entrySlice iter AllKeys Asc)
         return res
@@ -84,25 +71,14 @@ spec = do
 
 prop_RoundTrip :: (DbSchema, [DbRecord]) -> Property
 prop_RoundTrip (schema, records) = monadicIO $ do
-  r   <- run $ bracket initDB destroyDB $ \(Rs db _) -> do
-    _   <- write db def (map (tupleToBatchOp . (encodeRecord schema 1)) records)
-    res <- withIter db def (\iter -> SM.collect
-                                     $ SM.toList
+  r   <- run $ withTmpDb $ \(Rs db _) -> do
+    _   <- populate db (map (tupleToBatchOp . (encodeRecord schema 1)) records)
+    res <- withIter db def (\iter -> S.collect
+                                     $ S.toList
                                      $ withDecoded Record schema
                                      $ entrySlice iter AllKeys Asc)
     return res
   assert $ (sortWith ts <$> r) == Right (sortWith ts records)
-
-
-initDB :: IO Rs
-initDB = do
-  tmp <- getTemporaryDirectory
-  dir <- createTempDirectory tmp "leveldb-streaming-tests"
-  db  <- open dir defaultOptions { createIfMissing = True }
-  return $ Rs db dir
-
-destroyDB :: Rs -> IO ()
-destroyDB (Rs db dir) = unsafeClose db `finally` destroy dir defaultOptions
 
 ts :: DbRecord -> Integer
 ts (DbRecord i _) = i
