ec58b676aefe4706b6f24cea2c72c97fa979d734
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-15 22:29:34 +0200

    Convert chunk storage POC to tests

--- tests/Continuum/Storage/ChunkStorageSpec.hs
+++ tests/Continuum/Storage/ChunkStorageSpec.hs
@@ -12,22 +12,45 @@ import Continuum.Support.TmpDb
 import Database.LevelDB.Base     ( withIter, write, open, defaultOptions, createIfMissing, destroy )
 
 import Data.Default
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Catch    (MonadMask)
 import qualified Continuum.Stream as S
+import qualified Data.List as L
 
 spec :: Spec
 spec = do
 
   describe "Time Range Bounds" $ do
-    it "Case 1" $ do
+    it "Fetches all the items in the table" $ do
+      let bounds = addBounds AllTime (take 10 $ [encodeChunkKey i | i <- [0, 10..]])
+          input  = take 100 [(encodeChunkKey i, pack $ show i) | i <- [1..]]
       r <- withTmpDb $ \(Rs db _) -> do
-        populate db $ take 10 [Put (encodeChunkKey i) (pack $ show i) | i <- [100,200..]]
-        -- fetchChunks
-      1 `shouldBe` 2
+        populate db $ map (uncurry Put) input
+        fetchParts db bounds
+      r `shouldBe` (map snd input)
 
-a = withTmpDb $ \(Rs db _) -> do
-  let bounds = addBounds AllTime (take 10 $ [encodeChunkKey i | i <- [0, 10..]])
-  populate db $ take 100 [Put (encodeChunkKey i) (pack $ show i) | i <- [1..]]
-  mapM (fetchPart db) bounds
+    it "Fetches just a range" $ do
+      let bounds = addBounds (TimeBetween 15 55) ([encodeChunkKey i | i <- [20,30,40, 50]])
+          input  = take 100 [(encodeChunkKey i, pack $ show i) | i <- [1..]]
 
+      r <- withTmpDb $ \(Rs db _) -> do
+        populate db $ map (uncurry Put) input
+        fetchParts db bounds
+      r `shouldBe` [pack $ show i | i <- [15..54]]
+
+    it "Fetches just a range (given odd number of ranges)" $ do
+      let bounds = addBounds (TimeBetween 15 45) ([encodeChunkKey i | i <- [20,30,40]])
+          input  = take 100 [(encodeChunkKey i, pack $ show i) | i <- [1..]]
+
+      r <- withTmpDb $ \(Rs db _) -> do
+        populate db $ map (uncurry Put) input
+        fetchParts db bounds
+      r `shouldBe` [pack $ show i | i <- [15..44]]
+
+
+fetchParts :: (MonadMask m, MonadIO m) => DB -> [KeyRange] -> m [Value]
+fetchParts db ranges = L.concat <$> mapM (fetchPart db) ranges
+
+fetchPart :: (MonadMask m, MonadIO m) => DB -> KeyRange -> m [Value]
 fetchPart db range = withIter db def (\iter ->
-                                       S.toList $ S.map (\(_,y) -> y ) $ entrySlice iter range Asc)
+                                       S.toList $ S.map (\(_,y) -> y) $ entrySlice iter range Asc)
