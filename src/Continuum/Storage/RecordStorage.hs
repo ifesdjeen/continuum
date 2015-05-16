48bcad5bad31dea5d6af90a91a5bec0852911c3c
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-16 22:13:02 +0200

    Add "withField"

--- src/Continuum/Storage/RecordStorage.hs
+++ src/Continuum/Storage/RecordStorage.hs
@@ -13,5 +13,9 @@ import qualified Continuum.Stream as S
 runQuery :: (MonadMask m, MonadIO m, Monoid b) => DB -> KeyRange -> Fold Entry b -> m b
 runQuery db range (Fold f z0 e) = withIter db def (\iter -> fmap e $ S.foldl f z0 $ entrySlice iter range Asc)
 
--- withField :: (Monoid b) => (Fold ) -> (Fold Entry b)
--- withField = undefined
+withField :: (Monoid b) => FieldName -> (Fold DbValue b) -> Fold DbRecord (Maybe b)
+withField fieldName (Fold f z0 e) =
+  let valueFn      = getValue fieldName
+      wrapped z1 x = f <$> z1 <*> (valueFn x)
+  in
+   Fold wrapped (Just z0) (fmap e)
