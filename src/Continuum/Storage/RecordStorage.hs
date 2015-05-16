4360ce2630f15a8c71e1561414f30243a4c552cb
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-16 11:57:48 +0200

    Add "runQuery"

---
+++ src/Continuum/Storage/RecordStorage.hs
@@ -0,0 +1,19 @@
+module Continuum.Storage.RecordStorage where
+
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Catch    (MonadMask)
+
+import Continuum.Folds
+import Continuum.Types
+import Continuum.Serialization.Record
+import Continuum.Storage.GenericStorage
+
+import qualified Continuum.Stream as S
+
+Stream m Entry
+
+runQuery :: (MonadMask m, MonadIO m, Monoid b) => DB -> KeyRange -> Fold Entry b -> m b
+runQuery db range (Fold f z0 e) = withIter db def (\iter -> e <$> S.foldl f z0 $ entrySlice iter range Asc)
+
+-- withField :: (Monoid b) => (Fold ) -> (Fold Entry b)
+-- withField = undefined
