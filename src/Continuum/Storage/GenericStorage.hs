ed9a7b53cf61532f82bd865325ac2575941539a1
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-17 23:30:27 +0200

    Implement query runner

--- src/Continuum/Storage/GenericStorage.hs
+++ src/Continuum/Storage/GenericStorage.hs
@@ -1,6 +1,5 @@
 module Continuum.Storage.GenericStorage ( keySlice
                                         , entrySlice
-                                        , withDecoded
 
                                           -- LevelDB.Base
                                         , withIter
@@ -13,12 +12,9 @@ module Continuum.Storage.GenericStorage ( keySlice
                                         , def) where
 
 import Control.Monad.IO.Class
-import Continuum.Serialization.Record ( decodeRecord )
+import Continuum.Serialization.Record ( decodeRecord, getValue' )
 import Database.LevelDB.Streaming     ( keySlice, entrySlice )
 import Database.LevelDB.Base          ( withIter, write, open, defaultOptions, createIfMissing, destroy )
 import Data.Default                   ( def )
 import qualified Database.LevelDB.Streaming as S
 import Continuum.Types
-
-withDecoded :: (Applicative m, MonadIO m) => Decoding -> DbSchema -> Stream m Entry -> Stream m (DbErrorMonad DbRecord)
-withDecoded decoding schema = S.map (decodeRecord decoding schema)
--- src/Continuum/Storage/RecordStorage.hs
+++ src/Continuum/Storage/RecordStorage.hs
@@ -2,6 +2,7 @@ module Continuum.Storage.RecordStorage where
 
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Catch    (MonadMask)
+import Control.Monad     ( liftM2 )
 
 import Continuum.Folds
 import Continuum.Types
@@ -10,22 +11,39 @@ import Continuum.Storage.GenericStorage
 
 import qualified Continuum.Stream as S
 
-runQuery :: (MonadMask m, MonadIO m, Monoid b) => DB -> KeyRange -> Fold Entry b -> m b
-runQuery db range (Fold f z0 e) = withIter db def (\iter -> fmap e $ S.foldl f z0 $ entrySlice iter range Asc)
+withDecoded :: (Applicative m, MonadIO m) => Decoding -> DbSchema -> Stream m Entry -> Stream m (DbErrorMonad DbRecord)
+withDecoded decoding schema = S.map (decodeRecord decoding schema)
+
+withField :: (Applicative m, MonadIO m) => FieldName -> Stream m (DbErrorMonad DbRecord) -> Stream m (DbErrorMonad DbValue)
+withField field = S.map (>>= \i -> getValue' field i)
+
+runQuery :: (MonadMask m, MonadIO m, Monoid b)
+         => DB -> KeyRange
+         -> Decoding -> DbSchema
+         -> FieldName
+         -> Fold DbValue b -> m (DbErrorMonad b)
+runQuery db range decoding schema field (Fold f z0 e)=
+  withIter db def (\iter -> fmap (fmap e)
+                                                            $ S.foldl (\z1 x -> f <$> z1 <*> x) (Right z0)
+                                                            $ withField field
+                                                            $ withDecoded decoding schema
+                                                            $ entrySlice iter range Asc)
+
+-- runQuery :: (MonadMask m, MonadIO m, Monoid b) => DB -> KeyRange -> Fold Entry b -> m b
+-- runQuery db range (Fold f z0 e) = withIter db def (\iter -> fmap e $ S.foldl f z0 $ entrySlice iter range Asc)
+
 
 -- TODO Figure out how MonadMask works
-withField :: (Monoid b) => FieldName -> (Fold DbValue b) -> Fold DbRecord (Maybe b)
-withField fieldName (Fold f z0 e) =
-  let valueFn      = getValue fieldName
-      wrapped z1 x = f <$> z1 <*> (valueFn x)
-  in
-   Fold wrapped (Just z0) (fmap e)
+-- withField :: (Monoid b) => FieldName -> (Fold DbValue b) -> FoldM DbRecord DbErrorMonad b
+-- withField fieldName (Fold f z0 e) =
+--   let valueFn      = getValue' fieldName
+--       wrapped z1 x = f z1 <$> (valueFn x)
+--   in
+--    FoldM wrapped z0 (fmap e)
 
-decoded :: (Monoid b) => Decoding -> DbSchema -> (Fold DbRecord b) -> Fold Entry (Maybe b) -- WRONG
-decoded decoding schema (Fold f z0 e) =
-  let valueFn           = decodeRecord decoding schema
-      wrapped z1 x      = f <$> z1 <*> (valueFn x)
-      toMaybe (Left _)  = Nothing
-      toMaybe (Right a) = Just a
-  in
-   Fold wrapped (Right z0) (\y -> toMaybe $ fmap e y)
+-- decoded :: (Monoid b) => Decoding -> DbSchema -> (Fold DbRecord b) -> Fold Entry (DbErrorMonad b) -- WRONG
+-- decoded decoding schema (Fold f z0 e) =
+--   let valueFn           = decodeRecord decoding schema
+--       wrapped z1 x      = f <$> z1 <*> (valueFn x)
+--   in
+--    Fold wrapped (Right z0) (\y -> fmap e y)
