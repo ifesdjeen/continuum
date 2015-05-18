d8d1621a0e194356c5d3ebdbf1064328e17ca610
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-18 23:28:43 +0200

    Cleanup

--- src/Continuum/Storage/RecordStorage.hs
+++ src/Continuum/Storage/RecordStorage.hs
@@ -1,8 +1,8 @@
 module Continuum.Storage.RecordStorage where
 
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Catch    (MonadMask)
-import Control.Monad     ( liftM2 )
+import Control.Monad.IO.Class ( MonadIO )
+import Control.Monad.Catch    ( MonadMask )
+import Control.Monad          ( liftM2 )
 
 import Continuum.Folds
 import Continuum.Types
@@ -24,26 +24,7 @@ runQuery :: (MonadMask m, MonadIO m, Monoid b)
          -> Fold DbValue b -> m (DbErrorMonad b)
 runQuery db range decoding schema field (Fold f z0 e)=
   withIter db def (\iter -> fmap (fmap e)
-                                                            $ S.foldl (\z1 x -> f <$> z1 <*> x) (Right z0)
-                                                            $ withField field
-                                                            $ withDecoded decoding schema
-                                                            $ entrySlice iter range Asc)
-
--- runQuery :: (MonadMask m, MonadIO m, Monoid b) => DB -> KeyRange -> Fold Entry b -> m b
--- runQuery db range (Fold f z0 e) = withIter db def (\iter -> fmap e $ S.foldl f z0 $ entrySlice iter range Asc)
-
-
--- TODO Figure out how MonadMask works
--- withField :: (Monoid b) => FieldName -> (Fold DbValue b) -> FoldM DbRecord DbErrorMonad b
--- withField fieldName (Fold f z0 e) =
---   let valueFn      = getValue' fieldName
---       wrapped z1 x = f z1 <$> (valueFn x)
---   in
---    FoldM wrapped z0 (fmap e)
-
--- decoded :: (Monoid b) => Decoding -> DbSchema -> (Fold DbRecord b) -> Fold Entry (DbErrorMonad b) -- WRONG
--- decoded decoding schema (Fold f z0 e) =
---   let valueFn           = decodeRecord decoding schema
---       wrapped z1 x      = f <$> z1 <*> (valueFn x)
---   in
---    Fold wrapped (Right z0) (\y -> fmap e y)
+                            $ S.foldl (\z1 x -> f <$> z1 <*> x) (Right z0)
+                            $ withField field
+                            $ withDecoded decoding schema
+                            $ entrySlice iter range Asc)
