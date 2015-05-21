cf8f1821b039299a1b5d910d8e6d76d875fd571f
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-21 20:26:26 +0200

    Simplify RecordStorage

--- src/Continuum/Storage/RecordStorage.hs
+++ src/Continuum/Storage/RecordStorage.hs
@@ -14,27 +14,15 @@ import qualified Continuum.Stream as S
 withDecoded :: (Applicative m, MonadIO m) => Decoding -> DbSchema -> Stream m Entry -> Stream m (DbErrorMonad DbRecord)
 withDecoded decoding schema = S.map (decodeRecord decoding schema)
 
-withField :: (Applicative m, MonadIO m) => FieldName -> Stream m (DbErrorMonad DbRecord) -> Stream m (DbErrorMonad DbValue)
-withField field = S.map (>>= \i -> getValue' field i)
+-- withField :: (Applicative m, MonadIO m) => FieldName -> Stream m (DbErrorMonad DbRecord) -> Stream m (DbErrorMonad DbValue)
+-- withField field = S.map (>>= \i -> getValue' field i)
 
 fieldQuery :: (MonadMask m, MonadIO m, Monoid b)
          => DB -> KeyRange
          -> Decoding -> DbSchema
-         -> FieldName -> Fold DbValue b
+         -> Fold DbRecord b
          -> m (DbErrorMonad b)
-fieldQuery db range decoding schema field (Fold f z0 e) =
-  withIter db def (\iter -> fmap (fmap e)
-                            $ S.foldl (\z1 x -> f <$> z1 <*> x) (Right z0)
-                            $ withField field
-                            $ withDecoded decoding schema
-                            $ entrySlice iter range Asc)
-
-recordQuery :: (MonadMask m, MonadIO m, Monoid b)
-               => DB -> KeyRange
-            -> Decoding -> DbSchema
-            -> Fold DbRecord b
-            -> m (DbErrorMonad b)
-recordQuery db range decoding schema (Fold f z0 e) =
+fieldQuery db range decoding schema (Fold f z0 e) =
   withIter db def (\iter -> fmap (fmap e)
                             $ S.foldl (\z1 x -> f <$> z1 <*> x) (Right z0)
                             $ withDecoded decoding schema
