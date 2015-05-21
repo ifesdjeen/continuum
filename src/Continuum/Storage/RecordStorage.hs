1d318d930a01ad8bac920b2c19a629487d2a0eea
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-21 20:26:57 +0200

    Cleanup

--- src/Continuum/Storage/RecordStorage.hs
+++ src/Continuum/Storage/RecordStorage.hs
@@ -14,9 +14,6 @@ import qualified Continuum.Stream as S
 withDecoded :: (Applicative m, MonadIO m) => Decoding -> DbSchema -> Stream m Entry -> Stream m (DbErrorMonad DbRecord)
 withDecoded decoding schema = S.map (decodeRecord decoding schema)
 
--- withField :: (Applicative m, MonadIO m) => FieldName -> Stream m (DbErrorMonad DbRecord) -> Stream m (DbErrorMonad DbValue)
--- withField field = S.map (>>= \i -> getValue' field i)
-
 fieldQuery :: (MonadMask m, MonadIO m, Monoid b)
          => DB -> KeyRange
          -> Decoding -> DbSchema
@@ -24,6 +21,6 @@ fieldQuery :: (MonadMask m, MonadIO m, Monoid b)
          -> m (DbErrorMonad b)
 fieldQuery db range decoding schema (Fold f z0 e) =
   withIter db def (\iter -> fmap (fmap e)
-                            $ S.foldl (\z1 x -> f <$> z1 <*> x) (Right z0)
+                            $ S.fold[<64;17;13Ml (\z1 x -> f <$> z1 <*> x) (Right z0)
                             $ withDecoded decoding schema
                             $ entrySlice iter range Asc)
