9642d9219a0825f4636aebb763bdc7fe0231d89d
Alex Petrov <oleksandr.petrov@gmail.com>
2015-06-04 23:34:58 +0200

    Use withDB vs openDb

--- src/Continuum/Storage/SystemStorage.hs
+++ src/Continuum/Storage/SystemStorage.hs
@@ -4,8 +4,8 @@ import Continuum.Types
 import Continuum.Serialization.Schema
 import Continuum.Storage.GenericStorage
 -- import Data.ByteString
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Catch    (MonadMask)
+import Control.Monad.IO.Class ( MonadIO, liftIO )
+import Control.Monad.Catch    ( MonadMask )
 import System.Process         ( system )
 import qualified Data.ByteString.Char8  as BS
 import qualified Continuum.Stream       as S
@@ -24,8 +24,7 @@ createDatabase sysDb dbName sch = do
   _    <- LDB.put sysDb def dbName (encodeSchema sch)
   return OK
 
-openDb :: String -> DbName -> IO DB
-openDb path dbName = do
-  _  <- system ("mkdir " ++ path)
-  db <- LDB.open (path ++ "/" ++ (BS.unpack dbName)) def
-  return db
+withDb :: (MonadMask m, MonadIO m) => String -> DbName -> (DB -> m a) -> m a
+withDb path dbName action = do
+  _  <- liftIO $ system ("mkdir " ++ path)
+  LDB.withDB (path ++ "/" ++ (BS.unpack dbName)) def action
